c6fcdb96febc654b281db6feb2280ee42292216f
DXVII <david.pham.d@gmail.com>
2024-08-21 17:12:27 +1000

    update mapping function for Examples Section

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,48 +1,53 @@
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
-
-function SectionCoreConcepts() {
-    return <ul> 
-        {CORE_CONCEPTS.map( (concept) => 
-            <CoreConcept
-                key={concept.title}
-                title={concept.title}
-                description={concept.description}
-                imgSrc={concept.image}
-                imgAlt={concept.title}
-            />
-        )}
-    </ul>;
-}
-
-function SectionExamples() {
-    <menu>
-        <button>Components</button>
-        <button>JSX</button>
-        <button>Props</button>
-        <button>State</button>
-    </menu>
-}
 
 function App() {
   return (
     <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-        <section id="core-concepts">
-            <h2>Core Concepts</h2>
-            <SectionCoreConcepts />
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <SectionExamples />
-        </section>
-      </main>
+        <Header />
+        <main>
+            <h2>Time to get started!</h2>
+
+            <section id="core-concepts">
+                <h2>Core Concepts</h2>
+                <SectionCoreConcepts />
+            </section>
+
+            <section id="examples">
+                <h2>Examples</h2>
+                <SectionExamples />
+            </section>
+
+        </main>
     </div>
   );
 }
+
+function SectionCoreConcepts() {
+    return (
+        <ul> 
+            {CORE_CONCEPTS.map( (concept) => 
+                <CoreConcept
+                    title={concept.title}
+                    description={concept.description}
+                    imgSrc={concept.image}
+                    imgAlt={concept.title}
+                />
+            )}
+        </ul>
+    );
+}
+
+function SectionExamples() {
+    return (
+        <menu>
+            {Object.keys(EXAMPLES).map( 
+                (key) => <button>{EXAMPLES[key].title}</button>
+            )}
+        </menu>)
+}
+
 
 export default App;
 
