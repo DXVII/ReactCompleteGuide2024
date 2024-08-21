77e6c04335ca8dac421b217f9cce25e13462ea07
DXVII <david.pham.d@gmail.com>
2024-08-21 22:09:14 +1000

    fix prettier

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,10 +1,13 @@
 export default function TabButton({ title, selectedTopic, onSelect }) {
-  const isSelected = title.toLowerCase() === selectedTopic.toLowerCase();
-  return (
-    <li>
-      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
-        {title}
-      </button>
-    </li>
-  );
+    const isSelected = title.toLowerCase() === selectedTopic.toLowerCase();
+    return (
+        <li>
+            <button
+                className={isSelected ? 'active' : undefined}
+                onClick={onSelect}
+            >
+                {title}
+            </button>
+        </li>
+    );
 }
