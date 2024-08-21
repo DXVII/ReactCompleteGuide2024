f0e866b1bd6896fdd56d480eefb9c42c7b78cdba
DXVII <david.pham.d@gmail.com>
2024-08-21 22:00:48 +1000

    prettier tab 4

--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -1,11 +1,10 @@
-export default function CoreConcept({title, description, imgSrc, imgAlt}){
-    const uniqueId = `core-concept-${title}`
-    return(
+export default function CoreConcept({ title, description, imgSrc, imgAlt }) {
+  const uniqueId = `core-concept-${title}`;
+  return (
     <li>
       <img src={imgSrc} alt={imgAlt} />
       <h3>{title}</h3>
       <p>{description}</p>
     </li>
-  )
-
+  );
 }
