2c323cd7e9847f186ba31a764b9020ac1899636b
DXVII <david.pham.d@gmail.com>
2024-08-21 23:18:05 +1000

    refactor

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,76 +1,17 @@
-import { useState } from 'react';
-import { CORE_CONCEPTS, EXAMPLES } from './data';
+
 import Header from './components/Header';
-import CoreConcept from './components/CoreConcept';
-import TabButton from './components/TabButton';
-import TabContent from './components/TabContent';
+import SectionCoreConcepts from './components/SectionCoreConcepts';
+import SectionExamples from './components/SectionExamples';
 
-function App() {
-    // States:
-    const [selectedTopic, setSelectedTopic] = useState('');
-    console.log(`Current selectedTopic: ${selectedTopic}`);
-
-    // Event Handlers:
-    function handleClickExample(key) {
-        setSelectedTopic(key);
-    }
-
-    // App:
+export default function App() {
     return (
         <>
             <Header />
             <main>
                 <h2>Time to get started!</h2>
-
-                <section id="core-concepts">
-                    <h2>Core Concepts</h2>
-                    <SectionCoreConcepts />
-                </section>
-
-                <section id="examples">
-                    <h2>Examples</h2>
-                    <SectionTabButtons
-                        selectedTopic={selectedTopic}
-                        handleClickExample={handleClickExample}
-                    />
-                    <TabContent selectedTopic={selectedTopic} />
-                </section>
+                <SectionCoreConcepts />
+                <SectionExamples />
             </main>
         </>
     );
 }
-
-function SectionCoreConcepts() {
-    return (
-        <ul>
-            {CORE_CONCEPTS.map((concept) => (
-                <CoreConcept
-                    key={concept.title}
-                    title={concept.title}
-                    description={concept.description}
-                    imgSrc={concept.image}
-                    imgAlt={concept.title}
-                />
-            ))}
-        </ul>
-    );
-}
-
-function SectionTabButtons({ selectedTopic, handleClickExample }) {
-    return (
-        <menu>
-            {/* note: onSelect is an arbitrary name for passing
-              the event handler to the "onClick" defined in TabButton */}
-            {Object.keys(EXAMPLES).map((key) => (
-                <TabButton
-                    key={EXAMPLES[key].title}
-                    title={EXAMPLES[key].title}
-                    selectedTopic={selectedTopic}
-                    onSelect={() => handleClickExample(key)}
-                />
-            ))}
-        </menu>
-    );
-}
-
-export default App;
