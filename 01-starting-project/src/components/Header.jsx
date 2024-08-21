9366dcb18e3f436a8af57ce9f39e37a48ea25e4b
DXVII <david.pham.d@gmail.com>
2024-08-21 18:47:35 +1000

    react descriptions simplified

--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -5,12 +5,13 @@ function  genRandomInt(max) {
 }
 
 export default function Header() {
+  const adjective = reactDescriptions[genRandomInt(reactDescriptions.length)]
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInt(reactDescriptions.length)]} React concepts you will need for almost any app you are going to build!
+        {adjective} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
