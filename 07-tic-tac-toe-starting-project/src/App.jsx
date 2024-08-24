d4e85c5ce4efd8e2b9e187329ef3479e49ca4896
DXVII <david.pham.d@gmail.com>
2024-08-24 21:20:40 +1000

    default sketch structure

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,34 @@
 function App() {
-    return <h1>React Tic-Tac-Toe</h1>;
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="players">
+                            <span className="player-name">Player 1</span>
+                            <span className="player-symbol">X</span>
+                        </span>
+                        <button className="players">Edit Name</button>
+                    </li>
+                    <li>
+                        <span className="players">
+                            <span className="player-name">Player 2</span>
+                            <span className="player-symbol">O</span>
+                        </span>
+                        <button className="players">Edit Name</button>
+                    </li>
+                </ol>
+                <center>
+                    <div id="game-board">
+                        Game Board
+                    </div> 
+                </center>
+            </div>
+            <center>
+                <button>Reset Game</button>
+            </center>
+        </main>
+    )
 }
 
 export default App;
