fb6fe349755d0a6a11429f79aaaf7490960279ac
DXVII <david.pham.d@gmail.com>
2024-09-23 18:20:06 +1000

    negative duration bug

--- 05-investment-calculator/src/components/Results.jsx
+++ 05-investment-calculator/src/components/Results.jsx
@@ -14,8 +14,6 @@ export default function Results({ userInput }) {
             {investmentDetailHead(investmentResult)}
             {investmentDetailBody(investmentResult)}
         </table>
-        
-        
     )
 }
 
