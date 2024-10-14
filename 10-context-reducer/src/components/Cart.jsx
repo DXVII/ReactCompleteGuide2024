aae4af45b42c0e3ba70392f57ec6cdc4f204f7c7
DXVII <david.pham.d@gmail.com>
2024-10-14 18:21:32 +1100

    clean up

--- 10-context-reducer/src/components/Cart.jsx
+++ 10-context-reducer/src/components/Cart.jsx
@@ -9,41 +9,36 @@ export default function Cart() {
     )
     const formattedTotalPrice = `$${totalPrice.toFixed(2)}`
 
+    const addItemButton = (item) => (
+        <button onClick={() => updateCartItemQuantity(item.id, 1)}>+</button>
+    )
+    const subtractItemButton = (item) => (
+        <button onClick={() => updateCartItemQuantity(item.id, -1)}>-</button>
+    )
+
+    const renderItemsList = items.map((item) => {
+        const formattedPrice = `$${item.price.toFixed(2)}`
+
+        return (
+            <li key={item.id}>
+                <div>
+                    <span>{item.name}</span>
+                    <span> ({formattedPrice})</span>
+                </div>
+                <div className="cart-item-actions">
+                    {subtractItemButton(item)}
+                    <span>{item.quantity}</span>
+                    {addItemButton(item)}
+                </div>
+            </li>
+        )
+    })
     return (
         <div id="cart">
-            {items.length === 0 && <p>No items in cart!</p>}
-            {items.length > 0 && (
-                <ul id="cart-items">
-                    {items.map((item) => {
-                        const formattedPrice = `$${item.price.toFixed(2)}`
-
-                        return (
-                            <li key={item.id}>
-                                <div>
-                                    <span>{item.name}</span>
-                                    <span> ({formattedPrice})</span>
-                                </div>
-                                <div className="cart-item-actions">
-                                    <button
-                                        onClick={() =>
-                                            updateCartItemQuantity(item.id, -1)
-                                        }
-                                    >
-                                        -
-                                    </button>
-                                    <span>{item.quantity}</span>
-                                    <button
-                                        onClick={() =>
-                                            updateCartItemQuantity(item.id, 1)
-                                        }
-                                    >
-                                        +
-                                    </button>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
+            {items.length > 0 ? (
+                <ul id="cart-items">{renderItemsList}</ul>
+            ) : (
+                <p>No items in cart!</p>
             )}
             <p id="cart-total-price">
                 Cart Total: <strong>{formattedTotalPrice}</strong>
