1e5a4447ad4ce3270904ec42ce4c8803f83bc526
DXVII <david.pham.d@gmail.com>
2024-10-14 17:48:48 +1100

    consuming context corrected for default export bug

--- 10-context-reducer/src/components/Header.jsx
+++ 10-context-reducer/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { useContext } from 'react'
-import CartContext from '../store/shopping-cart-context.jsx'
+import { CartContext } from '../store/shopping-cart-context.jsx'
 import CartModal from './CartModal.jsx'
 export default function Header({ onUpdateCartItemQuantity }) {
     const modal = useRef()
